4077db6258939febad63d82b8d9ae1a2d8996efc
LinirZamir <alzcorp@gmail.com>
2022-10-14 01:20:07 -0400

    Added HW section 1

--- SGD/gradient_decent.py
+++ SGD/gradient_decent.py
@@ -9,7 +9,7 @@ for i in range(1000):
     x = np.random.randn(d)
     y = true_w.dot(x) + np.random.randn() ## Plus some noise
     points.append((x,y))
-
+  
 ####################################################
 ## Helper functions
 def F(w):
@@ -18,14 +18,32 @@ def F(w):
 def dF(w):
     return sum(2*(w.dot(x) - y)*x for x,y in points) / len(points)
 
+def sF(w, x, y):
+    return (w.dot(x) - y)**2
+    
+def sdF(w, x, y):
+    return 2*(w.dot(x) - y)*x
+
 ####################################################
 ## Gradient Descent
-def gradientDecent(step):
+def gradientDecent(step, size):
     w = np.zeros(d)
-    for t in range(1000):
+    for t in range(size):
         value = F(w)
         gradient = dF(w)
         w = w - step * gradient
         print("Iteration: {}, w = {}".format(t, w))
 
-gradientDecent(0.01)
+####################################################
+## Stochastic Gradient Descent
+def stochasticGradientDecent(step,size):
+    w = np.zeros(d)
+    for t in range(size):
+        for x,y in points:
+            value = sF(w,x,y)
+            gradient = sdF(w,x,y)
+            w = w - step * gradient
+        print("Iteration: {}, w = {}".format(t, w))
+
+stochasticGradientDecent(0.01, 20)
+gradientDecent(0.01, 20)
