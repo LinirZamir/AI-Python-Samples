69062b68a93f139cdb5a661de5508fa77d05739a
LinirZamir <alzcorp@gmail.com>
2022-10-14 16:26:47 -0400

    Hinge Function Added

--- SGD/AnotherExample.py
+++ SGD/AnotherExample.py
@@ -2,19 +2,33 @@ import numpy as np
 
 
 def phi(x):
-    return np.array([1, x])
+    return np.array(x)
 
+def phi_1(x):
+    return np.array([1, x])
 
 def initializeFunction(d):
     return np.zeros(d)
 
+# Stochastic GD
+
 
 def trainLoss(w, points):
-    return 1/len(points) * sum((w.dot(phi(x)) - y)**2 for x, y in points)
+    return 1/len(points) * sum((w.dot(phi_1(x)) - y)**2 for x, y in points)
 
 
 def dTrainLoss(w, points):
-    return 1/len(points) * sum(2*(w.dot(phi(x)) - y) * phi(x) for x, y in points)
+    return 1/len(points) * sum(2*(w.dot(phi_1(x)) - y) * phi(x) for x, y in points)
+
+# Hinge Lost
+
+
+def hingeLost(w, points):
+    return 1/len(points) * sum(max(1-w.dot(phi(x)) * y, 0) for x, y in points)
+
+
+def dHingeLost(w, points):
+    return 1/len(points) * sum(-phi(x)*y if 1 - w.dot(phi(x)) * y > 0 else 0 for x, y in points)
 
 
 def gradientDescent(initialize, f, df, eta, points, d):
@@ -32,9 +46,18 @@ def main():
         (2, 3),
         (4, 3),
     ]
+
+    HingeTrainExample = [
+        ((0, 2), 1),
+        ((-2, 0), 1),
+        ((1, -1), -1),
+    ]
     step = 0.1
-    gradientDescent(initializeFunction, trainLoss, dTrainLoss,
-                    step, trainExample, 2)
+    dimension = 2
+    ##gradientDescent(initializeFunction, trainLoss, dTrainLoss, step, trainExample, 2)
+    # Hinge GD
+    gradientDescent(initializeFunction, hingeLost,
+                    dHingeLost, step, HingeTrainExample, dimension)
 
 
 main()
